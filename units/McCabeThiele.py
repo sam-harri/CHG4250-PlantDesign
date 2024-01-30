73625e79620e60ee04ab149183b086a58384657b
sam-harri <sharr125@uottawa.ca>
2024-01-30 12:17:35 -0500

    project structure and OOP outline

---
+++ units/McCabeThiele.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class McCabeThiele:
+    def __init__(self, equilibrium_cruve: List[float]) -> None:
+        pass
+
+    def getFlows(
+        coordinate: [float, float], show_plot: bool = False
+    ) -> [List[float], List[float]]:
+        pass
+
+    def __plot() -> None:
+        pass
---
+++ units/Stripping.py
@@ -0,0 +1,6 @@
+from units.UnitBaseClass import UnitInterface
+
+
+class Stripping(UnitInterface):
+    def __init__(self) -> None:
+        super().__init__()
